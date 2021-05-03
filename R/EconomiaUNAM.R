2eb1943351f046f32c88ec0f31809f9bae278779
Cesar Gerardo Hernández Vargas <66124391+cghv94@users.noreply.github.com>
2021-05-03 00:01:52 -0500

    Update EconomiaUNAM.R

--- R/EconomiaUNAM.R
+++ R/EconomiaUNAM.R
@@ -6,7 +6,7 @@
   if(rstudioapi::versionInfo()$version < 1.3){
     warning("Tu versión de R es muy antigua, por favor actualiza el programa para poder ejecutar los tutoriales")
   } else {
-    packageStartupMessage("Hola, este es el paquete de tutoriales de R de la Facultad de Economía, UNAM.\nEsta es una lista de los tutoriales disponibles:\n 1) tutorial\n 2) son todos, jajaja")
+    packageStartupMessage("Hola, este es el paquete de tutoriales de R de la Facultad de Economía, UNAM.\nEsta es una lista de los tutoriales disponibles:\n 1) boceto-tutorial\n 2) tutorial")
 
   }
 }
