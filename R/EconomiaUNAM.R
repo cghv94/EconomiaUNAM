f832b44b7bf85ffb933be8ac0a2bba06ea97571f
Duque-de-Sealand <amieva.daniel@comunidad.unam.mx>
2021-05-26 14:53:22 -0500

    Prerendered tutorials
    to test learnr::available_tutorials

--- R/EconomiaUNAM.R
+++ R/EconomiaUNAM.R
@@ -6,7 +6,8 @@
   if(rstudioapi::versionInfo()$version < 1.3){
     warning("Tu versión de R es muy antigua, por favor actualiza el programa para poder ejecutar los tutoriales")
   } else {
-    packageStartupMessage("Hola, este es el paquete de tutoriales de R de la Facultad de Economía, UNAM.\nEsta es una lista de los tutoriales disponibles:\n 1) boceto-tutorial\n 2) tutorial")
+    packageStartupMessage("Hola, este es el paquete de tutoriales de R de la Facultad de Economía, UNAM.\n
+                          Esta es una lista de los tutoriales disponibles:")
 
   }
 }
